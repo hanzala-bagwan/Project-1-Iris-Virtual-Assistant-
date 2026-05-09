7e8e10c3e19715d70ab781eb90347a524dfa8556
hanzala-bagwan <hanzalaabagwan@gmail.com>
2026-05-09 15:04:34 +0530

    New Commit on 9/5/26

--- main.py
+++ main.py
@@ -2,8 +2,31 @@ import speech_recognition as sr
 import webbrowser
 import time
 import musiclib
+import requests
 
 r = sr.Recognizer()
+
+
+def ask_ai(prompt):
+
+    url = "http://localhost:11434/api/generate"
+
+    data = {
+        "model": "phi3",
+        "prompt": f"You are Iris, a voice assistant. Answer in 2 short sentences: {prompt}",
+        "stream": False,
+        "options": {
+            "num_predict": 80
+        }
+    }
+
+    try:
+        response = requests.post(url, json=data)
+        result = response.json()
+        return result["response"]
+
+    except:
+        return "Sorry, AI is not available right now."
 
 def speak(text): 
     import pyttsx3
@@ -20,24 +43,32 @@ def processCommand(voicecmd):
     if "open youtube" in voicecmd:
         speak("Opening Youtube")
         webbrowser.open_new_tab("https://youtube.com")
+   
     elif "open google" in voicecmd:
         speak("Opening Google")
         webbrowser.open_new_tab("https://google.com")   
+   
     elif "open linkedin" in voicecmd:
         speak("Opening LinkedIn")
         webbrowser.open_new_tab("https://linkedin.com")     
+   
     elif "open instagram" in voicecmd:
         speak("Opening Instagram")
         webbrowser.open_new_tab("https://instagram.com")
+   
     elif "open facebook" in voicecmd:
         speak("Opening Facebook")
         webbrowser.open_new_tab("https://facebook.com")  
+   
     elif voicecmd.lower().startswith("play"):
        songs = voicecmd.lower().replace("play","").strip()
        if songs in musiclib.song:
            link = musiclib.song[songs]
            speak(f"Playing {songs}")
            webbrowser.open_new_tab(link)
+    else:
+     answer = ask_ai(voicecmd)
+     speak(answer)       
 
 
   
@@ -80,4 +111,4 @@ if __name__ == "__main__":
             except Exception as e:
                 print("Unexpected error:", repr(e))
 
-            time.sleep(0.5)    
+            time.sleep(0.5)
